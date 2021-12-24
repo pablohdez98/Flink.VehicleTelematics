522438a57c95580425d3e06f884444e7acb82d95
Pablo Hernández Carrascosa <pablohc224@gmail.com>
2021-12-24 02:56:34 +0100

    Read csv and map it

--- src/main/java/master/VehicleTelematics.java
+++ src/main/java/master/VehicleTelematics.java
@@ -1,7 +1,50 @@
 package master;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.*;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import java.util.Iterator;
+import java.util.Objects;
+
 public class VehicleTelematics {
+    static String outFolderPath;
     public static void main(String[] args) {
 
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Input file and output folder should be given");
+        }
+        String inFilePath = args[0];
+        outFolderPath = args[1];
+
+        // set up the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // get input data
+        DataStream<String> source = env.readTextFile(inFilePath);
+
+        // Map the data in tuples of 8, and assign timestamps and watermarks
+        SingleOutputStreamOperator<Event> events = source
+                .map((MapFunction<String, Event>) Event::new)
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forMonotonousTimestamps()
+                        .withTimestampAssigner((element, timeStamp) -> element.event.f0 * 1000));
+        // Execution
+        try {
+            env.execute("VehicleTelematics");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
